8313abf0baa0b51e8b5cc3b6bd076146ea0c1a4a
chrisislin <Christopherlin3@gmail.com>
2021-02-07 14:44:09 -0800

    seperated components

---
+++ src/Provider.jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, useRef } from 'react';
+import { BOARD_ROW, BOARD_COL, FIXED_COLOR } from './constants';
+import PathFinder from 'algo/index.js';
+
+const Context = createContext();
+const _begin = { x: 7, y: 2 };
+const _end = { x: 1, y: 9 };
+const _board = new Array(BOARD_ROW);
+for(let i=0; i<BOARD_ROW; i++){
+  _board[i] = [];
+  for(let j=0; j<BOARD_COL; j++){
+    _board[i][j] = {};
+  }
+}
+_board[_begin.x][_begin.y] = { color: FIXED_COLOR, visit: true };
+_board[_end.x][_end.y].color = FIXED_COLOR;
+
+const Provider = (props) => {
+
+  const [begin, setBegin] = useState(_begin);
+  const [end, setEnd] = useState(_end);
+  const [board, setBoard] = useState(_board);
+  const pathFinder = useRef(PathFinder.dijkstra);
+
+  const clear = () => {
+    setBoard(_board);
+  };
+
+  return (
+    <Context.Provider value={{
+      begin, setBegin,
+      end, setEnd,
+      board, setBoard,
+      pathFinder
+    }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
+
+export { Context, Provider };
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
-export const BOARD_ROW = 13;
-export const BOARD_COL = 13;
+export const BOARD_ROW = 16;
+export const BOARD_COL = 30;
 export const VISIT_COLOR = 'blue';
 export const SHORT_COLOR = 'red';
 export const FIXED_COLOR = 'black';
+
+export const DIJKSTRA = 'dijkstra';
+export const FLOYD = 'floyd';
