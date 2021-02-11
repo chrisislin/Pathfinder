20b5fa64835a77c96235cd8106d082b83f73c7d4
chrisislin <Christopherlin3@gmail.com>
2021-02-10 18:21:21 -0800

    added grey boxes

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,20 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from 'Provider';
+import { CLICK_COLOR, INITIAL_COLOR } from 'constants.js';
 import './Board.scss';
 
-const Board = () => {
+const Board = ({ name, ref }) => {
   const context = useContext(Context);
-  const { board } = context;
+  const { board, setBoard } = context;
+  const [clicking, setClicking] = useState(false);
+
+  const onMouseDown = () => {
+    setClicking(true);
+  }
+
+  const onMouseUp = () => {
+    setClicking(false);
+  }
+
+  const changeColorHandler = (ridx, cidx) => {
+    return () => {
+      const copy = JSON.parse(JSON.stringify(board));
+      copy[ridx][cidx].color = CLICK_COLOR;
+      setBoard(copy);
+    };
+  }
 
   return (
-    <div className="board">
+    <div className="board" onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
       {board.map((row, ridx) => (
         <div className="board_row" key={ridx} style={{ display: 'flex', justifyContent: 'center' }}>
           {row.map((col, cidx) => {
+            let onMouseMove = null;
+            if (board[ridx][cidx].color === INITIAL_COLOR && clicking) {
+              onMouseMove = changeColorHandler(ridx, cidx);
+            }
+
             return (
-            <div className="board_col" key={2 * ridx + cidx} style={{
-              backgroundColor: (board[ridx][cidx].color || null)
-            }} />
+              <div className="board_col"
+                key={2 * ridx + cidx}
+                onMouseMove={onMouseMove}
+                onClick={changeColorHandler(ridx, cidx)}
+                style={{
+                  backgroundColor: (board[ridx][cidx].color || null),
+                  transition: (clicking ? 'none' : 'background-color 0.3s linear')
+                }} />
             )
           })}
           <br />
--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,9 @@ export const BOARD_COL = 30;
 export const VISIT_COLOR = 'blue';
 export const SHORT_COLOR = 'green';
 export const FIXED_COLOR = 'black';
+export const INITIAL_COLOR = 'red';
+export const CLICK_COLOR = 'grey';
+
 
 export const DIJKSTRA = 'dijkstra';
 export const BELLMAN_FORD = 'bellman_ford';
