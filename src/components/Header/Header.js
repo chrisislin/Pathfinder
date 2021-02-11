c5ba48a3f2b08dad511f82a9e78455e872a01175
chrisislin <Christopherlin3@gmail.com>
2021-02-10 18:42:26 -0800

    changed some header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { DIJKSTRA, BELLMAN_FORD } from 'constants.js';
+import { DIJKSTRA, BELLMAN_FORD, SHORT_COLOR } from 'constants.js';
 import { Context } from 'Provider';
 import PathFinder from 'algo/index.js';
 import './Header.scss';
@@ -7,12 +7,18 @@ import { clear } from 'sisteransi';
 
 const Header = () => {
   const [type, setType] = useState(DIJKSTRA);
+  const [delay, setDelay] = useState(300);
   const context = useContext(Context);
-  const { begin, end, board, setBoard, pathFinder } = context;
+  const { begin, end, board, setBoard, pathFinder, clear } = context;
 
   const onSelectChange = (e) => {
     setType(e.target.value);
   };
+
+  const onDelayChange = (e) => {
+    setDelay(e.target.value);
+  };
+
 
   const onVisualize = (_e) => {
     pathFinder.current = new PathFinder[type]({
@@ -20,7 +26,7 @@ const Header = () => {
       end,
       board,
       setState: setBoard,
-      delay: 300
+      delay
     });
     pathFinder.current.execute();
   };
@@ -35,14 +41,30 @@ const Header = () => {
     }
   }, [board, pathFinder, end]);
 
+  useEffect(() => {
+    if (board[end.x][end.y].color === SHORT_COLOR) {
+      pathFinder.current.clearTimers();
+    }
+  }, [board, pathFinder, end]);
+
   return (
     <header className="header">
       <select className="header_select" onChange={onSelectChange}>>
         <option value={DIJKSTRA} defaultChecked={true}>Dijkstra</option>
         <option value={BELLMAN_FORD}>Bellman-Ford</option>
       </select>
-      <button className="header_visualize" onClick={onVisualize}>
-        Click on me
+      <select className="header_select" onChange={onDelayChange} defaultValue={300}>
+        <option value={550}>slowest</option>
+        <option value={450}>slowest</option>
+        <option value={300}>default</option>
+        <option value={150}>fast</option>
+        <option value={50}>fastest</option>
+      </select>
+      <button className="header_button" onClick={onVisualize}>
+        Visualize this
+      </button>
+      <button className="header_button" onClick={onClear}>
+        Clear
       </button>
     </header>
   );
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const BOARD_ROW = 16;
 export const BOARD_COL = 30;
 export const VISIT_COLOR = 'blue';
-export const SHORT_COLOR = 'green';
+export const SHORT_COLOR = 'gold';
 export const FIXED_COLOR = 'black';
 export const INITIAL_COLOR = 'red';
 export const CLICK_COLOR = 'grey';
