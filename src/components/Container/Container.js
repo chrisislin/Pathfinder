0670a3da41d070f3bde9f099343f9dd247185970
chrisislin <Christopherlin3@gmail.com>
2021-02-10 18:07:48 -0800

    slightly changed the container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Header from '../Header/Header';
 import Board from '../Board/Board';
 import { Provider } from 'Provider';
+import './Container.scss';
 
 const Container = () => {
 
   return (
-    <Provider>
-      <Header />
-      <Board />
-    </Provider>
+    <>
+      <header className="header">
+        <h1 className="header_title">Pathfinder</h1>
+      </header>
+      <Provider>
+        <Header />
+        <Board />
+      </Provider>
+    </>
   );
 };
 
